66e7f64d98fd018283e51d48c510073ed63d9299
5amaa <135070852+5amaa@users.noreply.github.com>
2023-12-10 16:43:16 +0200

    DataSeeding

--- Talabat/Talabat.Api/Program.cs
+++ Talabat/Talabat.Api/Program.cs
@@ -5,7 +5,7 @@ namespace Talabat.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -22,6 +22,26 @@ namespace Talabat.Api
             });
 
             var app = builder.Build();
+
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                var dbcontext = services.GetRequiredService<StoreContext>();
+                await dbcontext.Database.MigrateAsync();
+
+                await StoreContextSeed.SeedAsync(dbcontext);
+
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "Error during Migragtion");
+
+            }
+
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
---
+++ Talabat/Talabat.Repository/Data/StoreContextSeed.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+
+namespace Talabat.Repository.Data
+{
+    public static class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext dbcontext)
+        {
+            if(!dbcontext.ProductBrands.Any())
+            {
+                var brandData = File.ReadAllText("../Talabat.Repository/Data/DataSeed/brands.json");
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandData);
+                if(brands?.Count > 0)
+                {
+                    foreach (var brand in brands)
+                    {
+                        await dbcontext.AddAsync<ProductBrand>(brand);  
+ 
+                    }await dbcontext.SaveChangesAsync();
+
+
+                }
+            }
+
+            if (!dbcontext.ProductTypes.Any())
+            {
+                var typeData = File.ReadAllText("../Talabat.Repository/Data/DataSeed/types.json");
+                var types = JsonSerializer.Deserialize<List<ProductType>>(typeData);
+                if (types?.Count > 0)
+                {
+                    foreach (var type in types)
+                    {
+                        await dbcontext.AddAsync<ProductType>(type);
+
+                    }
+                    await dbcontext.SaveChangesAsync();
+
+
+                }
+            }
+
+            if (!dbcontext.Products.Any())
+            {
+                var productData = File.ReadAllText("../Talabat.Repository/Data/DataSeed/products.json");
+                var products = JsonSerializer.Deserialize<List<Product>>(productData);
+
+                if (products?.Count > 0)
+                {
+                    foreach (var product in products)
+                    {
+                        await dbcontext.AddAsync<Product>(product);
+                    }
+                    await dbcontext.SaveChangesAsync();
+                }
+            }
+        }
+
+    }
+}
