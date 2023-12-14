3007f2f9a81db3f5dfde6a79499b9b69786c7c3a
5amaa <135070852+5amaa@users.noreply.github.com>
2023-12-14 11:53:52 +0200

    push Repository

---
+++ Talabat/Talabat.Repository/Repository/GenaricRepository.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities;
+using Talabat.Core.Repository;
+using Talabat.Repository.Data;
+
+namespace Talabat.Repository.Repository
+{
+    public class GenaricRepository<T> : IGenaricRepository<T> where T : BaseEntity
+    {
+        public StoreContext dbContext;
+        public GenaricRepository(StoreContext _dbContext) {
+            dbContext = _dbContext;
+        }
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await dbContext.Set<T>().FindAsync(id);
+        }
+    }
+}
